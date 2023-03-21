7538da75a235507a71a819d946d8eb17bed6132d
carabajal-facundo <facarabajal@hotmail.com>
2023-03-20 22:26:54 -0300

    Se agrego lo solicitado, aun que no se sabe el motivo de por que no reproduce las demas lines que se encuentran debajo de la linea 98

--- js/programa.js
+++ js/programa.js
@@ -62,17 +62,65 @@ function crearTabla(array){
     document.write("<table>");
         document.write("<thead>");
             document.write("<tr>");
-                document.write("<td></td>");
+                document.write("<td>Producto</td>");
+                document.write("<td>Categoria</td>");
+                document.write("<td>Precio</td>");
             document.write("</tr>");
         document.write("</thead>");
-
         document.write("<tbody>");
-            document.write("<tr>");
-                document.write("<td></td>");
-            document.write("</tr>");
-        document.write("</tbody>");
+        for(let i=0; i < listaProductos.length; i++){
+              document.write("<tr>");
+              document.write("<td>"+ array[i].nombreProducto +"</td>");
+              document.write("<td>"+ array[i].categoria +"</td>");
+              document.write("<td>$ "+ array[i].precio +"</td>");
+              document.write("</tr>");
+        }
+      document.write("</tbody>");
     document.write("</table>");
-    for(let i=0; i< listaProductos.length();i++){
-     
-    }
 }
+
+function encontrarProducto(array,nombre){
+  let encontrado=false;
+  for(let i=0; i<array.length; i++){
+    if(array[i].nombreProducto==nombre) encontrado = true;
+  }
+  if(encontrado) return encontrado;
+  else {
+    return encontrado;
+  }
+}
+
+crearTabla(listaProductos);
+
+
+let protectores = listaProductos.filter(prod => prod.categoria.includes("Protector solar"));
+document.write("<br>Tabla con los productos que son 'Protectores solares'<br><br>");
+crearTabla(protectores);
+
+
+let buscarSerum = listaProductos.find(prod => prod.categoria.includes("Sérum"));
+document.write("<br>Tabla con el producto de categoria 'Sérum '<br><br>");
+document.write("<table>");
+  document.write("<thead>");
+    document.write("<tr>");
+      document.write("<td>Producto</td>");
+      document.write("<td>Categoria</td>");
+      document.write("<td>Precio</td>");
+    document.write("</tr>");
+  document.write("</thead>");
+  document.write("<tbody>");
+    document.write("<tr>");
+      document.write("<td>"+ buscarSerum.nombreProducto +"</td>");
+        document.write("<td>"+ buscarSerum.categoria +"</td>");
+        document.write("<td>$ "+ buscarSerum.precio +"</td>");
+      document.write("</tr>");
+  document.write("</tbody>");
+document.write("</table>");
+
+if(encontrarProducto(listaProductos, 'Bruma')) {
+  let products = listaProductos.filter(prod => prod.nombreProducto.includes("Bruma"));
+  document.write("<br>Tabla con los productos llamados 'Bruma'<br><br>");
+  crearTabla(protectores);
+}
+else alert("No se encontro el producto");
+
